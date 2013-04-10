f3cb1cf7f66ff09e4fa7a15b7ca03dc04d0eab6d
Jonathan Camenisch <jonathan@camenisch.net>
2013-04-10 10:33:55 -0400

    Install pry-rescue for debugging

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,4 +3,6 @@ require 'minitest/autorun'
 require 'minitest-matchers'
 require 'pry'
 
+require 'pry-rescue/minitest' if ENV['PRY_RESCUE']
+
 Dir[File.expand_path('../support/**/*.rb', __FILE__)].each { |f| require f }
